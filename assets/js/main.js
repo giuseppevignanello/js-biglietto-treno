7dc709f25afa6cab9bedea8c21ea520ed34adc61
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-08 16:29:55 +0100

    fix if and else if

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,27 +36,25 @@ document.getElementById("price").innerHTML = (`€ ${userPrice}`);
 console.log (`€ ${userPrice}`);
 
 //check if the user is entitled to a discount with if/els
-
-const discount20 = ((userPrice / 100) * 20);
 const discount40 = ((userPrice / 100) * 20);
 
-    //if he is a minor calculate 20 percent of the price and subtract it from the price
-const discountedMinor = (userPrice - discount20 )
+    //if he is a minor calculate 20 percent of the price and subtract it from the price, if else he is over 65 calculate 40 percent of the price and subtract it from the price
+
+
 if (userAge < 18) {
     
+    const discount20 = ((userPrice / 100) * 20);
     userPrice = discountedMinor; 
     
     console.log(`€ ${userPrice}`)
 
-    //round the price to cents with Math.round(UserFinalPrice * 100)/100
-    
     userPrice = Math.round(userPrice * 100) /100
     document.getElementById("price").innerHTML = (`€ ${userPrice}`)
     console.log(`€ ${userPrice}`)
 
 }
 
-    //if he is over 65 calculate 40 percent of the price and subtract it from the price
+    //if 
 
 const discountedOver65 = (userPrice - discount40 )
 if (userAge >=65) {
