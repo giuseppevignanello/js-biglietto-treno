6819081172b0c3e1dc9db2259997d18d518af5a8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-08 15:10:41 +0100

    add tools

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,16 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+
+
+//Tools
+
+// - prompt
+// - const
+// - console.log
+// -* multiplication
+// - / division 
+// if else
+//Math.round
